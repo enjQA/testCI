94a70b64f46aeccd1efd7ad16b9b65634d6e60cd
Dmytro Kulyk <tesstsetsetset@mail.com>
2022-02-08 21:54:31 +0200

    CI with ESlint TS

--- _tests_/specs/cat.spec.ts
+++ _tests_/specs/cat.spec.ts
@@ -1,12 +1,14 @@
 import { Cat } from "../code/cat";
 
-const catName: string = 'Brownie';
+const catName = 'Brownie';
 const cat = new Cat(catName, 'white');
 
-test('testing move function', () => {
-    expect(cat.move(10)).toBe(`${catName} moved 10m`);
-})
+describe('cat unit tests', () => {
+    test('testing move function', () => {
+        expect(cat.move(10)).toBe(`${catName} moved 10m`);
+    })
 
-test('testing say function', () => {
-    expect(cat.say()).toBe(`Cat ${catName} says meow`);
+    test('testing say function', () => {
+        expect(cat.say()).toBe(`Cat ${catName} says meow`);
+    })
 })
